d9cd0525cd1fac6220ebe6dcbed60b846faf1a53
Martin <martin.ter.haak@gmail.com>
2015-10-12 18:00:51 +0200

    Changed storing of coefficients to most to least significant, added methods for comparing, fixed gcd methods. Fixed commutative operations.

--- objects/PolynomialModP.py
+++ objects/PolynomialModP.py
@@ -7,7 +7,7 @@ class PolynomialModP(list):
     """Polynomial mod prime p
 
     Attributes:
-        coefs (list): list of IntegerModP, sorted from least to most significant
+        coefs (list): list of IntegerModP, sorted from most to least significant
         p (int): prime
     """
 
@@ -22,13 +22,13 @@ class PolynomialModP(list):
             raise ValueError("Argument coefficients is not a list.")
         coefs = []
         skip_zeros = True  # The first * number of zero coefficients should not be included
-        for c in coefficients:
-            if c % p != 0 or not skip_zeros or len(coefficients) == 1:
+        for i, c in enumerate(coefficients):
+            if c % p != 0 or not skip_zeros or i == len(coefficients) - 1:
                 # all coefficients are forced modulo of p
                 if type(c) is IntegerModP:
-                    coefs = [IntegerModP(c.n % p, p)] + coefs
+                    coefs += [IntegerModP(c.n % p, p)]
                 elif type(c) is int:
-                    coefs = [IntegerModP(c, p)] + coefs  # int is converted to IntegerModP
+                    coefs += [IntegerModP(c, p)]  # int is converted to IntegerModP
                 else:
                     raise ValueError("Coefficient " + str(c) + " is not an IntegerModP or int.")
             if c % p != 0 and skip_zeros:
@@ -71,11 +71,7 @@ class PolynomialModP(list):
     # Clone self to prevent mutation
     def _clone(self):
         """Clone self."""
-        return PolynomialModP(self.int_coefs(), self.p)
-
-    def int_coefs(self):
-        """Return coefficients as a list of int, sorted from most to least significant."""
-        return list(map(int, reversed(self.coefs)))
+        return PolynomialModP(self.coefs, self.p)
 
     def degree(self):
         """Return the degree."""
@@ -86,20 +82,19 @@ class PolynomialModP(list):
         """Return the image of x."""
         result = 0
         t = 1  # term
-        for c in self.coefs:
+        for c in reversed(self.coefs):
             result += c.n * t
             t *= x
         return result
 
-    @_check_p
     # Long division
     def long_divide(self, other):
         """Long divide by other PolynomialModP, returns (quotient, remainder) as tuple."""
         q = PolynomialModP([0], self.p)  # quotient
         r = self._clone()  # remainder
         d = other  # divisor
-        while r.degree() >= d.degree() and r != 0:
-            f_coefs = [int(r.coefs[-1] / d.coefs[-1])]
+        while r.degree() > d.degree() or (r.degree() == d.degree() and r.coefs[-1] >= d.coefs[-1]):
+            f_coefs = [int(r.coefs[0] / d.coefs[0])]  # coefficient of term
             f_coefs += (r.degree() - d.degree()) * [0]  # Pad with zero's to denote term
             f = PolynomialModP(f_coefs, self.p)  # factor
             r -= f * d
@@ -109,20 +104,15 @@ class PolynomialModP(list):
     @_check_p
     # Polynomial mod p gcd using Euclidian algorithm
     def gcd(self, other):
-        """Calculate the gcd with other PolynomialModP."""
+        """Calculate the gcd with other PolynomialModP, returns (gcd, x, y) as tuple s.t. gcd = x * self + y * other."""
         a = self._clone()
         b = other._clone()
-        while b > [1]:
-            a, b = b, a % b
-        return a if not b else PolynomialModP([1], self.p)
-        # def gcd_rec(a, b):
-        #     if a == PolynomialModP([0], self.p):
-        #         return b
-        #     else:
-        #         q, r = b / a
-        #         return gcd_rec(r, a)
-        #
-        # return gcd_rec(a, b)
+        x, y, u, v = 0, 1, 1, 0
+        while a != 0:
+            q, r = b.long_divide(a)
+            m, n = x - u * q, y - v * q
+            b, a, x, y, u, v = a, r, u, v, m, n
+        return b, x, y
 
     # Polynomial mod p congruence with other polynomial modulo mod
     def congruent(self, other, k):
@@ -133,55 +123,60 @@ class PolynomialModP(list):
     # Polynomial mod p addition (sum)
     def __add__(self, other):
         """Add other PolynomialModP."""
-        coefs = [(i + j) % self.p for i, j in itertools.zip_longest(self, other, fillvalue=0)]
-        return PolynomialModP(list(reversed(coefs)), self.p)  # init requires int list in reverse
+        d, e = [0] * (len(other) - len(self)), [0] * (len(self) - len(other))  # Padding to make lists equal length
+        coefs = [(i + j) % self.p for i, j in zip(d + self.coefs, e + other.coefs)]
+        return PolynomialModP(coefs, self.p)
 
     # Makes add operation commutative, result is in both cases a PolynomialModP
+    @_check_p
     def __radd__(self, other):
-        return self.__add__(other)
+        return other.__add__(self)
 
     @_check_p
     # Polynomial mod p subtraction (difference)
     def __sub__(self, other):
         """Subtract other PolynomialModP."""
-        coefs = [(i - j) % self.p for i, j in itertools.zip_longest(self, other, fillvalue=0)]
-        return PolynomialModP(list(reversed(coefs)), self.p)
+        d, e = [0] * (len(other) - len(self)), [0] * (len(self) - len(other))  # Padding to make lists equal length
+        coefs = [(i - j) % self.p for i, j in zip(d + self.coefs, e + other.coefs)]
+        return PolynomialModP(coefs, self.p)
 
     # Makes subtract operation commutative
+    @_check_p
     def __rsub__(self, other):
-        return self.__rsub__(other)
+        return other.__sub__(self)
 
     @_check_p
     # Polynomial mod p multiplication (scalar multiple & product)
     def __mul__(self, other):
         """Multiply with other PolynomialModP (or IntegerModP/int for scalar multiple)."""
-        coefs = [0] * (self.degree() + other.degree() + 1)
+        coefs = (self.degree() + other.degree() + 1) * [0]
         for i in range(0, len(self)):
             for j in range(0, len(other)):
-                coefs[i + j] += int(self.coefs[i]) * int(other.coefs[j])
-                coefs[i + j] %= self.p
-        return PolynomialModP(list(reversed(coefs)), self.p)
+                coefs[i + j] += self.coefs[i] * other.coefs[j]
+        return PolynomialModP(coefs, self.p)
 
     # Makes multiply operation commutative
+    @_check_p
     def __rmul__(self, other):
-        return self.__rmul__(other)
+        return other.__mul__(self)
 
+    @_check_p
     # Division operation makes use of long division
     def __truediv__(self, other):
         """Divide by other PolynomialModP."""
         return self.long_divide(other)[0]
 
+    @_check_p
     # Modulo operation makes use of long division
     def __mod__(self, other):
         """Modulo by other PolynomialModP."""
         return self.long_divide(other)[1]
 
-    #
+    # Power operation
     def __pow__(self, power):
+        """Raise to the power of other PolynomialModP."""
         if power == 0:
-            return PolynomialModP([0], self.p)
-        elif power == 1:
-            return self
+            return PolynomialModP([1], self.p)
         else:
             return self * self.__pow__(power - 1)
 
@@ -202,6 +197,22 @@ class PolynomialModP(list):
             return False
         return super().__ne__(other)
 
+    @_check_p
+    def __lt__(self, other):
+        super().__lt__(other)
+
+    @_check_p
+    def __le__(self, other):
+        super().__le__(other)
+
+    @_check_p
+    def __gt__(self, other):
+        super().__gt__(other)
+
+    @_check_p
+    def __ge__(self, other):
+        super().__ge__(other)
+
     # Constant polynomials can be converted to int
     def __int__(self):
         if self.degree() == 0:
@@ -214,11 +225,11 @@ class PolynomialModP(list):
         if not self.coefs:  # If no coefficients
             return "<null> (mod " + str(self.p) + ")"
         terms = []
-        for i in range(0, len(self.coefs)):
-            if self.coefs[i] != 0 or len(self) == 1:
+        for i, c in enumerate(reversed(self.coefs)):
+            if c != 0 or len(self) == 1:
                 term = ""
-                if self.coefs[i] != 1 or i == 0:
-                    term += str(self.coefs[i].n)
+                if c != 1 or i == 0:
+                    term += str(c.n)
                 if i > 0:
                     term += "x"
                 if i >= 2:
@@ -228,4 +239,4 @@ class PolynomialModP(list):
         return "{0} (mod {1})".format(" + ".join(terms), str(self.p))
 
     def __repr__(self):
-        return "PolynomialModP({0}, {1})".format(str(self.int_coefs()), str(self.p))
+        return "PolynomialModP({0}, {1})".format(str(self.coefs), str(self.p))
