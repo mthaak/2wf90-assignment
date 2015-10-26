5205bada14b90ffc4a08f8badd6fb0ebba69d8a0
Martin <martin.ter.haak@gmail.com>
2015-10-26 18:00:03 +0100

    Delete test_systemModP.py

--- tests/test_systemModP.py
+++
@@ -1,8 +0,0 @@
-from unittest import TestCase
-
-from objects.SytemModP import SystemModP
-
-
-class TestSystemModP(TestCase):
-    # TODO Implement test cases, dont forget test primality p test
-    pass
